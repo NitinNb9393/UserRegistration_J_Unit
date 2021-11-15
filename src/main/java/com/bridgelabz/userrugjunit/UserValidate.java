f4715e1041103ec214c2a495e27a73aaa58cb34d
NitinNb9393 <nitinbabar9393@gmail.com>
2021-11-15 16:21:53 +0530

    Added:Passwor with special charactor

--- src/main/java/com/bridgelabz/userrugjunit/UserValidate.java
+++ src/main/java/com/bridgelabz/userrugjunit/UserValidate.java
@@ -19,57 +19,76 @@ public class UserValidate {
 	public static boolean firstNameValidator() {
 		UserValidate uservalidate = new UserValidate();
 		System.out.println("Enter the first name: ");
-		String fName = uservalidate.sc.nextLine();
+		String fName = UserValidate.sc.nextLine();
 		return Pattern.matches(uservalidate.NAME_VALIDATOR, fName);
 	}
 
 	public static boolean lastNameValidator() {
 		UserValidate uservalidate = new UserValidate();
 		System.out.println("Enter the Last name: ");
-		String lName = uservalidate.sc.nextLine();
-		return Pattern.matches(uservalidate.NAME_VALIDATOR, lName);
+		String lName = UserValidate.sc.nextLine();
+		return Pattern.matches(uservalidate.LAST_NAME_VALIDATOR, lName);
 	}
 
 	public static boolean emailValidator() {
 		UserValidate uservalidate = new UserValidate();
 		System.out.println("Enter the Email address: ");
-		String email = uservalidate.sc.nextLine();
+		String email = UserValidate.sc.nextLine();
 		return Pattern.matches(uservalidate.EMAIL_VALIDATOR, email);
 	}
 
 	public static boolean mobNumValidator() {
 		UserValidate uservalidate = new UserValidate();
 		System.out.println("Enter the Mobile number: ");
-		String mobNum = uservalidate.sc.nextLine();
+		String mobNum = UserValidate.sc.nextLine();
 		return Pattern.matches(uservalidate.MOBILE_NUM_VALIDATOR, mobNum);
 	}
 
 	public static boolean password1Validator() {
 		UserValidate uservalidate = new UserValidate();
 		System.out.println("Enter the Password: ");
-		String password1 = uservalidate.sc.nextLine();
+		String password1 = UserValidate.sc.nextLine();
 		return Pattern.matches(uservalidate.PASSWORD_RULE1_VALIDATOR, password1);
 	}
 
 	public static boolean password2Validator() {
 		UserValidate uservalidate = new UserValidate();
 		System.out.println("Enter the Password 2: ");
-		String password2 = uservalidate.sc.nextLine();
+		String password2 = UserValidate.sc.nextLine();
 		return Pattern.matches(uservalidate.PASSWORD_RULE2_VALIDATOR, password2);
 	}
 
 	public static boolean password3Validator() {
 		UserValidate uservalidate = new UserValidate();
 		System.out.println("Enter the Password 3: ");
-		String password3 = uservalidate.sc.nextLine();
+		String password3 = UserValidate.sc.nextLine();
 		return Pattern.matches(uservalidate.PASSWORD_RULE3_VALIDATOR, password3);
 	}
 
 	public static boolean password4Validator() {
 		UserValidate uservalidate = new UserValidate();
 		System.out.println("Enter the Password 4: ");
-		String password4 = uservalidate.sc.nextLine();
+		String password4 = UserValidate.sc.nextLine();
 		return Pattern.matches(uservalidate.PASSWORD_RULE4_VALIDATOR, password4);
 	}
 
+	String[] validEmail = { "abc@yahoo.com", "abc-100@yahoo.com", "abc.100@yahoo.com", "abc111@abc.com",
+			"abc-100@abc.net", "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com" };
+	String[] invalidEmail = { "abc", "abc@.com.my", "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com",
+			"abc()*@gmail.com", "abc@%*.com", "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com",
+			"abc@gmail.com.1a", "abc@gmail.com.aa.au" };
+	{
+		for (String value : validEmail) {
+			System.out.println(value);
+			System.out.println(Pattern.matches(
+					"^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$",
+					value));
+		}
+		for (String s : invalidEmail) {
+			System.out.println(s);
+			System.out.println(Pattern.matches(
+					"^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$",
+					s));
+		}
+	}
 }
