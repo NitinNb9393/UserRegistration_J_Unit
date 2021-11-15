9120296c9a8b11e9e41df73b055d718971c3d52f
NitinNb9393 <nitinbabar9393@gmail.com>
2021-11-15 16:34:41 +0530

    Added:Email sample validate

---
+++ src/main/java/com/bridgelabz/userrugjunit/MoodAnalyser.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.userrugjunit;
+
+public class MoodAnalyser {
+
+	public static String analyseMood(String mood) {
+		if (mood.toLowerCase().contains("happy")) {
+			return "Entry Successful";
+		} else if (mood.toLowerCase().contains("sad")) {
+			return "Entry Failed";
+		} else
+			return null;
+	}
+}
